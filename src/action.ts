095688e58cd8e17daac1c623a35e168289fd4946
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-11-23 14:04:59 +0000

    chore: test

--- src/action.ts
+++ src/action.ts
@@ -8,26 +8,32 @@ import { validateEnvironment } from "./utils/validate-env";
 async function runAction() {
     const validatedEnv = validateEnvironment(process.env as Record<string, string>, "action") as WorkflowEnv;
     process.env = validatedEnv as unknown as Record<string, string>;
-
     console.log("Validated environment (process.env):", process.env);
 
-    return await createActionsPlugin<
-        PluginSettings,
-        WorkflowEnv,
-        Command,
-        SupportedWebhookEvents & SupportedCustomEvents
-    >(
-        (context) => {
-            return runSymbiote<SupportedEvents, "action">(context)
-        },
-        {
-            envSchema: workflowEnvSchema,
-            postCommentOnError: true,
-            settingsSchema: pluginSettingsSchema,
-            logLevel: (process.env.LOG_LEVEL as LogLevel) ?? LOG_LEVEL.INFO,
-            kernelPublicKey: process.env.KERNEL_PUBLIC_KEY as string,
-            bypassSignatureVerification: true
-        })
+    try {
+        return await createActionsPlugin<
+            PluginSettings,
+            WorkflowEnv,
+            Command,
+            SupportedWebhookEvents & SupportedCustomEvents
+        >(
+            (context) => {
+                return runSymbiote<SupportedEvents, "action">(context)
+            },
+            {
+                envSchema: workflowEnvSchema,
+                postCommentOnError: true,
+                settingsSchema: pluginSettingsSchema,
+                logLevel: (process.env.LOG_LEVEL as LogLevel) ?? LOG_LEVEL.INFO,
+                kernelPublicKey: process.env.KERNEL_PUBLIC_KEY as string,
+                bypassSignatureVerification: true
+            })
+    } catch (error) {
+        console.trace(error);
+        console.error("Error creating actions plugin:", error);
+        process.exit(1);
+    }
+
 }
 
 runAction().catch((error) => {
