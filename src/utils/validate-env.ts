90b25252189fed7282239b663adb824104c38bed
Keyrxng <106303466+Keyrxng@users.noreply.github.com>
2025-11-23 13:00:10 +0000

    chore: test

--- src/utils/validate-env.ts
+++ src/utils/validate-env.ts
@@ -2,20 +2,21 @@ import { Value } from "@sinclair/typebox/value";
 import { workerEnvSchema, workflowEnvSchema } from "../types/env";
 import { SymbioteRuntime } from "../types";
 import { env as honoEnv } from "hono/adapter";
+import { StaticDecode } from "@sinclair/typebox";
 
 export function validateEnvironment(env: Parameters<typeof honoEnv>[0], runtime: SymbioteRuntime) {
   const schema = runtime === "worker" ? workerEnvSchema : workflowEnvSchema;
-  const cleanedEnv = Value.Clean(schema, env);
-  console.log("Cleaned environment:", cleanedEnv);
-
-  if(runtime === "action") {
-    try {
-      console.log("Process environment:", Deno.env.toObject());
-    }catch(error) {
-      console.error("Error getting process environment:", error);
+  let cleanedEnv: StaticDecode<typeof schema> | undefined;
+  try {
+    cleanedEnv = Value.Clean(schema, env) as StaticDecode<typeof schema>;
+  }catch(error: unknown) {
+    if(error instanceof Error) {
+      console.error("Error cleaning environment:", error.message);
+      throw new Error(`Invalid environment variables: ${error.message}`);
     }
+    console.error("Error cleaning environment:", error);
   }
-
+  console.log("Cleaned environment:", cleanedEnv);
 
   if (!Value.Check(schema, cleanedEnv)) {
     const errors = [...Value.Errors(schema, cleanedEnv)];
